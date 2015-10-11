d80f588ae116876ed48ce1028d2307fbc0d60644
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-10-11 16:14:55 -0300

    fix login AJAX, hide search bar if not logged in

--- source/services/sessionService.js
+++ source/services/sessionService.js
@@ -9,21 +9,20 @@
 
     // Returns a promise
     self.login = function(userForm) {
-      return
-        $http({
-          method: 'POST',
-          url: 'http://arqui8.ing.puc.cl/api/v1/auth/token',
-          data: $.param(userForm),
-          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).success(function(data, textStatus, xhr) {
-            $sessionStorage.currentUser = { 'username' : userForm.username,
-                                            'password' : userForm.password,
-                                            'token' : data.token };
-            $window.location.href = '/#/home';
-        }).error(function(data, textStatus, xhr) {
-            alert('fail');
-            $window.location.href = '/#/';
-        });
+      return $http({
+        method: 'POST',
+        url: 'http://arqui8.ing.puc.cl/api/v1/auth/token',
+        data: $.param(userForm),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).success(function(data, textStatus, xhr) {
+          $sessionStorage.currentUser = { 'username' : userForm.username,
+                                          'password' : userForm.password,
+                                          'token' : data.token };
+          $window.location.href = '/#/home';
+      }).error(function(data, textStatus, xhr) {
+          alert('fail');
+          $window.location.href = '/#/';
+      });
     };
 
     self.logout = function() {
