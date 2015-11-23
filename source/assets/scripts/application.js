7c03f70402bc757db3e61be7ae066c799ef4484f
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-11-22 21:55:07 -0300

    fix some CodeClimate issues

--- source/assets/scripts/application.js
+++ source/assets/scripts/application.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   // Check to see if the window is top if not then display button.
   $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
-      $('#scroll-to-top').show();
+      $('.scroll-to-top').show();
     } else {
-      $('#scroll-to-top').hide();
+      $('.scroll-to-top').hide();
     }
   });
 });
--- source/directives/newsDisplay.directive.js
+++ source/directives/newsDisplay.directive.js
@@ -44,7 +44,7 @@
     });
 
     vm.scrollUp = function() {
-      $('#scroll-to-top').on('click', function() {
+      $('.scroll-to-top').on('click', function() {
         $('html, body').animate({scrollTop : 0}, 800);
       });
     };
